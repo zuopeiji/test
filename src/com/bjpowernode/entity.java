b782ee24fc76316fc3884b13814183ef000d3732
zuo <zuo@bjpowernode.com>
2021-06-16 20:24:28 +0800

    ok

--- src/com/bjpowernode/entity.java
+++ src/com/bjpowernode/entity.java
@@ -5,4 +5,35 @@ public class entity {
     private Integer id;
     private String name;
 
+    public entity() {
+    }
+
+    public entity(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "entity{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
